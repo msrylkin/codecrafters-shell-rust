7750ad8c09b3ef251265b3aa02c129508a3451da
mrylkin <m.rylkin@itez.com>
2025-02-11 10:16:22 +0300

    lib

--- src/lib.rs
+++ src/lib.rs
@@ -2,26 +2,21 @@ use std::fs;
 
 pub fn check_dir_for_cmd_predicate<T: FnMut(&str) -> bool>(
     dir: &str,
-    // cmd: &str,
     mut predicate: T
 ) -> Vec<String> {
-    let dir = fs::read_dir(dir);
-
-    let mut vec: Vec<String> = vec![];
-
-    if let Ok(dir) = dir {
-        for path  in dir {
-            if let Ok(path_item) = path {
-                if let Some(last) = path_item.path().iter().last() {
-                    if predicate(last.to_str().unwrap()) {
-                        // println!("cmd: {}", last.to_string_lossy().to_string());
-                        // return Some(last.to_string_lossy().to_string());
-                        vec.push(last.to_string_lossy().to_string());
-                    }
-                } 
-            }
-        }
-    }
-
-    vec
+    fs::read_dir(dir)
+        .ok()
+        .and_then(|read_dir| {
+            read_dir.map(|dir_entry| {
+                dir_entry.ok().and_then(|ok_dir_entry| {
+                    ok_dir_entry
+                        .file_name()
+                        .to_str()
+                        .map(String::from)
+                        .filter(|str| predicate(str))
+                })
+            })
+            .collect()
+        })
+        .unwrap_or_default()
 }
